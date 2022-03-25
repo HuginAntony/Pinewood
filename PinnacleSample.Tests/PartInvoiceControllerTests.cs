8b2a71514e54f20ed5f09429458d77d7005b325d
Hugin Antony <HuginAntony@gmail.com>
2022-03-25 20:35:16 +0200

    Adding PinnacleSample.Tests project Upgrading PinnacleSample to .NET Framework 4.8

---
+++ PinnacleSample.Tests/PartInvoiceControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PinnacleSample.Tests
+{
+    [TestClass]
+    public class PartInvoiceControllerTests
+    {
+        [TestMethod]
+        public void Can_Create_Part_Invoice()
+        {
+        }
+    }
+}
---
+++ PinnacleSample.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PinnacleSample.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PinnacleSample.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c9d02c6-8e50-480f-9f2a-0061e241adba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
